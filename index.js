89f0068427504cd8a06201d5bd3fc4d9cfa2092b
杉本　諒 <wasp2123321@gmail.com>
2021-11-10 15:53:15 +0900

    Adding Another Object Type

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const { ApolloServer, gql } = require("apollo-server");
 const { db } = require("./db.js");
 
 const products = db.products;
+const categories = db.categories;
 
 const typeDefs = gql`
   type Query {
     hello: String
     products: [Product!]!
     product(id: ID!): Product
+    categories: [Category!]!
+    category(id: ID!): Category
   }
   type Product {
     id: String!
@@ -18,6 +21,10 @@ const typeDefs = gql`
     image: String!
     onSale: Boolean!
     categoryId: String!
+  }
+  type Category {
+    id: ID!
+    name: String!
   }
 `;
 
@@ -34,6 +41,17 @@ const resolvers = {
       const product = products.find((product) => product.id === productId);
       if (!product) return null;
       return product;
+    },
+    categories: () => {
+      return categories;
+    },
+    category: (parent, args, context) => {
+      const categoryId = args.id;
+      const category = categories.find(
+        (category) => category.id === categoryId
+      );
+      if (!category) return null;
+      return category;
     },
   },
 };
